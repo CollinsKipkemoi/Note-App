8b6ef3a5b356cf625296b21000305eb1d999ec83
Collins Kipkemoi <chemerilcollins@gmail.com>
2024-07-27 14:53:04 +0200

    Note cards

--- FrontEnd/Note-App/src/components/Dashboard.tsx
+++ FrontEnd/Note-App/src/components/Dashboard.tsx
@@ -1,10 +1,60 @@
 import Navbar from "./Navbar"
+import NoteCard from "./NoteCard/NoteCard"
+import './Dashboard.css'
 
 function Dashboard() {
   return (
     <div >
-        <Navbar />
-        <h1>Dashboard</h1>
+      <Navbar />
+      <div className="notes container ">
+        <NoteCard title="Test"
+          date="2021-05-01"
+          content="This is a test note"
+          tags={["#test"]}
+          isPinned={false}
+          onEdit={() => console.log("Edit")}
+          onDelete={() => console.log("Delete")}
+          onPin={() => console.log("Pin")}
+        
+        />
+        <NoteCard title="Test"
+          date="2021-05-01"
+          content="This is a test note"
+          tags={["#test"]}
+          isPinned={false}
+          onEdit={() => console.log("Edit")}
+          onDelete={() => console.log("Delete")}
+          onPin={() => console.log("Pin")}
+
+        />
+        <NoteCard title="Test"
+          date="2021-05-01"
+          content="This is a test note"
+          tags={["#test"]}
+          isPinned={false}
+          onEdit={() => console.log("Edit")}
+          onDelete={() => console.log("Delete")}
+          onPin={() => console.log("Pin")}
+        />
+        <NoteCard title="Test"
+          date="2021-05-01"
+          content="This is a test note"
+          tags={["#test"]}
+          isPinned={false}
+          onEdit={() => console.log("Edit")}
+          onDelete={() => console.log("Delete")}
+          onPin={() => console.log("Pin")}
+        />
+        <NoteCard title="Test"
+          date="2021-05-01"
+          content="This is a test note"
+          tags={["#test"]}
+          isPinned={false}
+          onEdit={() => console.log("Edit")}
+          onDelete={() => console.log("Delete")}
+          onPin={() => console.log("Pin")}
+        />
+      </div>
     </div>
   )
 }
---
+++ FrontEnd/Note-App/src/components/NoteCard/NoteCard.tsx
@@ -0,0 +1,44 @@
+import { MdOutlinePushPin } from "react-icons/md";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FiEdit2 } from "react-icons/fi";
+import './NoteCard.css'
+
+interface NoteCardProps {
+    title: string
+    date: string
+    content: string
+    tags: string[]
+    isPinned: boolean
+    onEdit: () => void
+    onDelete: () => void
+    onPin: () => void
+}
+
+function NoteCard({ title, date, content, tags, isPinned, onEdit, onDelete, onPin }: NoteCardProps) {
+    return (
+        <div className="card">
+            <div className="cardHeader">
+                <div className="title-date">
+                    <h2>{title}</h2>
+                    <p>{date}</p>
+                </div>
+                <MdOutlinePushPin className="pin" onClick={onPin} />
+            </div>
+            <div className="c-body">
+                <p>{content}</p>
+            </div>
+            <div className="c-footer">
+                <div className="tags">
+                    {tags.map(tag => <span key={tag} className="tag">{tag}</span>)}
+                </div>
+                <div className="actions">
+                    <FiEdit2 onClick={onEdit} />
+                    <AiOutlineDelete onClick={onDelete} />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default NoteCard
