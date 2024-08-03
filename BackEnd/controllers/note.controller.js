cb71b6fd803b2d10d35efc79f74ce13dc5644aa6
Collins Kipkemoi <chemerilcollins@gmail.com>
2024-08-03 23:15:19 +0200

    Fix: Edit note feature, had an issue with the userID

--- BackEnd/controllers/note.controller.js
+++ BackEnd/controllers/note.controller.js
@@ -1,5 +1,6 @@
 const Note = require("../models/note.model");
 
+// !ADD NOTE
 const addNote = async (req, res) => {
   const { title, content, tags } = req.body;
   const { user } = req.user;
@@ -31,6 +32,47 @@ const addNote = async (req, res) => {
   }
 };
 
+// !EDIT NOTE
+const editNote = async (req, res) => {
+  const { noteId } = req.params;
+  const { title, content, tags, isPinned } = req.body;
+  const { user } = req.user;
+  if (!title && !content && !tags) {
+    return res.status(400).json({
+      error: true,
+      message: "No changes made!",
+    });
+  }
+
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+    if (!note) {
+      return res.status(404).json({
+        error: true,
+        message: "Note not found!",
+      });
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned) note.isPinned = isPinned;
+
+    await note.save();
+    return res.status(200).json({
+      error: false,
+      message: "Note edited succesfully!",
+      note,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: true,
+      message: err.message,
+    });
+  }
+};
 module.exports = {
   addNote,
+  editNote,
 };
--- BackEnd/routes/note.routes.js
+++ BackEnd/routes/note.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { authenticateToken } = require("../utilities");
-const { addNote } = require("../controllers/note.controller");
+const { addNote, editNote } = require("../controllers/note.controller");
 
 router.post("/add-note", authenticateToken, addNote);
+router.put("/edit-note/:noteId", authenticateToken, editNote);
 
 module.exports = router;
